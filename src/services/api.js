275084987f15e0191d824e3cde3e3873707f57c7
toledomg <toledomg@yahoo.com.br>
2023-07-31 19:53:43 -0300

    :rocket: branch for deploy in vercel

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const localUrl = "http://localhost:3000";
 const deployUrl = "https://cdm-api.luchost.com/";
 export const api = axios.create({
-  baseURL: localUrl,
+  baseURL: deployUrl,
   timeout: 5000,
   headers: {
     "Access-Control-Allow-Origin": "*",
