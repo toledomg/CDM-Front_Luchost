fa91f75a5fb3fb6887a965d4786b5e4ef506d81c
toledomg <toledomg@yahoo.com.br>
2023-08-05 21:04:32 -0300

    :recycle: clean code

--- src/services/api.js
+++ src/services/api.js
@@ -3,10 +3,6 @@ import axios from "axios";
 const localUrl = "http://localhost:3000";
 const deployUrl = "https://cdm-api.luchost.com/";
 export const api = axios.create({
-
-
-
-
   baseURL: deployUrl,
   timeout: 25000,
 
