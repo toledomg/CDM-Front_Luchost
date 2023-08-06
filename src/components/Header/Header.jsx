edfa6b940ed98d00e6db67b3f77e1c4899f3e0e3
toledomg <toledomg@yahoo.com.br>
2023-08-06 13:02:37 -0300

    :recycle: deploy url api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,13 @@ import logo from "/logo.png";
 import { HeaderHubDash } from "./styles";
 function HeaderD({ exitPage }) {
   return (
-    <>
-      <HeaderHubDash>
-        {/* <span>CDM LucHost</span> */}
-        <img src={logo} title="Logo da LucHost"></img>
-        <Link onClick={exitPage} to="/home" title="Fazer Logout">
-          Sair
-        </Link>
-      </HeaderHubDash>
-    </>
+    <HeaderHubDash>
+      {/* <span>CDM LucHost</span> */}
+      <img src={logo} title="Logo da LucHost"></img>
+      <Link onClick={exitPage} to="/home" title="Fazer Logout">
+        Sair
+      </Link>
+    </HeaderHubDash>
   );
 }
 
