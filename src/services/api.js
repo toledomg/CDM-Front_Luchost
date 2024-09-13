e8206cc1728f46857658d604ac2a04f6e541e00c
toledomg <toledomg@yahoo.com.br>
2024-09-13 10:54:22 -0300

    bugFix: api

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,12 @@ import axios from "axios";
 const localUrl = import.meta.env.VITE_API_LOCAL_URL;
 const deployUrl = import.meta.env.VITE_API_DEPLOY_URL;
 
-// Use a variável VITE_ENV para controlar o ambiente
+// Use a variável VITE_ENV para controlar o ambiente ou `NODE_ENV` para produção
 const baseURL =
-  import.meta.env.VITE_ENV === "development" ? localUrl : deployUrl;
+  import.meta.env.VITE_ENV === "development" ||
+  import.meta.env.NODE_ENV === "development"
+    ? localUrl
+    : deployUrl;
 
 export const api = axios.create({
   baseURL: baseURL,
