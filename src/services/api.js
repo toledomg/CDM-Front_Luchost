491b4b4520ad34b426af8faa5d6689c4136b05c4
toledomg <toledomg@yahoo.com.br>
2024-09-13 10:37:17 -0300

    refactor: alter api for development or production in env

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const localUrl = import.meta.env.VITE_LOCAL_URL;
-const deployUrl = import.meta.env.VITE_DEPLOY_URL;
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  timeout: 25000,
+const localUrl = import.meta.env.VITE_API_LOCAL_URL;
+const deployUrl = import.meta.env.VITE_API_DEPLOY_URL;
 
+// Use a variável VITE_ENV para controlar o ambiente
+const baseURL =
+  import.meta.env.VITE_ENV === "development" ? localUrl : deployUrl;
+
+export const api = axios.create({
+  baseURL: baseURL,
+  timeout: 25000,
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": "true",
